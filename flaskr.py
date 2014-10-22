96c8042c89e663be53110bc55cea534ea932ebd0
unknown <manas.george@gatech.edu>
2014-10-21 20:57:30 -0400

    Fixed import; Added init_db()

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,5 @@
 import sqlite 3
-from flaskr import Flask, g, abort, flash, g
+from flask import Flask, g, abort, flash, g
 
 # Config
 DATABASE = '/tmp/flaskr.db'
@@ -11,3 +11,15 @@ SECRET_KEY = 'development key'
 # Create app
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with close(connect_db()) as db:
+		with app.open_resource('schema.sql', mode'r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+if __name__ == '__main__':
+	app.run()
